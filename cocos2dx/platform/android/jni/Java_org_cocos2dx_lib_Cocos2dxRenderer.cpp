89c97c9322de5752f39ebbcaefb63d22fbea2589
Vladimir Kovner <wolves3d@gmail.com>
2013-11-04 06:18:13 +0400

    android right wakeup thread (more)

--- cocos2dx/platform/android/jni/Java_org_cocos2dx_lib_Cocos2dxRenderer.cpp
+++ cocos2dx/platform/android/jni/Java_org_cocos2dx_lib_Cocos2dxRenderer.cpp
@@ -15,14 +15,12 @@ extern "C" {
     }
 
     JNIEXPORT void JNICALL Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeOnPause() {
-        CCApplication::sharedApplication()->applicationDidEnterBackground();
-
-        CCNotificationCenter::sharedNotificationCenter()->postNotification(EVENT_COME_TO_BACKGROUND, NULL);
+		CCDirector::sharedDirector()->RiseBackgroundEnterFlag();
     }
 
     JNIEXPORT void JNICALL Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeOnResume() {
         if (CCDirector::sharedDirector()->getOpenGLView()) {
-            CCApplication::sharedApplication()->applicationWillEnterForeground();
+			CCDirector::sharedDirector()->RiseForegroundEnterFlag();
         }
     }
 
