198f304ca1e0f39a78603f925775e429cde5cc41
Vladimir Kovner <wolves3d@gmail.com>
2014-10-06 23:57:50 +0400

    android assert now leads to crash

--- cocos2dx/platform/android/CCPlatformDefine.h
+++ cocos2dx/platform/android/CCPlatformDefine.h
@@ -3,28 +3,6 @@
 
 #include "android/log.h"
 
-#define CC_DLL
-
-#define CC_NO_MESSAGE_PSEUDOASSERT(cond)                        \
-    if (!(cond)) {                                              \
-        __android_log_print(ANDROID_LOG_ERROR,                  \
-                            "cocos2d-x assert",                 \
-                            "%s function:%s line:%d",           \
-                            __FILE__, __FUNCTION__, __LINE__);  \
-    }
-
-#define CC_MESSAGE_PSEUDOASSERT(cond, msg)                          \
-    if (!(cond)) {                                                  \
-        __android_log_print(ANDROID_LOG_ERROR,                      \
-                            "cocos2d-x assert",                     \
-                            "file:%s function:%s line:%d, %s",      \
-                            __FILE__, __FUNCTION__, __LINE__, msg); \
-    }
-
-#define CC_SYSTEM_ASSERT(cond) CC_NO_MESSAGE_PSEUDOASSERT(cond)
-
-#define CC_UNUSED_PARAM(unusedparam) (void)unusedparam
-
 /* Define NULL pointer value */
 #ifndef NULL
 #ifdef __cplusplus
@@ -33,5 +11,33 @@
 #define NULL    ((void *)0)
 #endif
 #endif
+
+#define CC_DLL
+
+#define CC_NO_MESSAGE_PSEUDOASSERT(cond)                        \
+    if (!(cond)) {                                              \
+        __android_log_print(ANDROID_LOG_ERROR,                  \
+                            "cocos2d-x assert",                 \
+                            "%s function:%s line:%d",           \
+                            __FILE__, __FUNCTION__, __LINE__);  \
+																\
+		volatile int* a = (int*)(NULL);							\
+		*a = 1;													\
+    }
+
+#define CC_MESSAGE_PSEUDOASSERT(cond, msg)                          \
+    if (!(cond)) {                                                  \
+        __android_log_print(ANDROID_LOG_ERROR,                      \
+                            "cocos2d-x assert",                     \
+                            "file:%s function:%s line:%d, %s",      \
+                            __FILE__, __FUNCTION__, __LINE__, msg); \
+																	\
+		volatile int* a = (int*)(NULL);								\
+		*a = 1;														\
+    }
+
+#define CC_SYSTEM_ASSERT(cond) CC_NO_MESSAGE_PSEUDOASSERT(cond)
+
+#define CC_UNUSED_PARAM(unusedparam) (void)unusedparam
 
 #endif /* __CCPLATFORMDEFINE_H__*/
